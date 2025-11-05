c152ad9ae9f5fd2fb4917c0c00e8a2c34a92368a
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-11-05 14:34:58 -0300

    Security#8
    Adicionado o filtro com a chave/token JWT, configurado na classe SecurityFilter que recupera do header da requisição o token e valida ele e já verifica a lista de autoridade do usuario

--- src/main/java/com/projeto/app_hackaton/config/SecurityConfiguration.java
+++ src/main/java/com/projeto/app_hackaton/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.projeto.app_hackaton.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,12 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -25,6 +30,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize.requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/projeto/app_hackaton/config/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.projeto.app_hackaton.config;
+
+import com.projeto.app_hackaton.repository.UsuarioRepository;
+import com.projeto.app_hackaton.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recoverToken(request);
+        if (token != null){
+            var login = tokenService.validateToken(token);
+            UserDetails user = usuarioRepository.findByEmail(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null){
+            return null;
+        }
+        authHeader.replace("Bearer ", "");
+        return authHeader;
+    }
+}
