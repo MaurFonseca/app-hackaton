3bcce7f6dc5a0e02a65909f6b78a070a9b5bdc83
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-11-05 12:48:34 -0300

    Security #2
    Continuação da configuração do security, adicionado Repository com método que retorna um userDetails a partir de encontrar o usuário pelo login, nesse caso está sendo o atributo "email".
    
    Além de adicionado o Authorization Service que vai se alimentar desse repository, implementar o UserDetailsService que vai ter o método que vai resgatar o username/login no caso "email" do usuario.
    
    Além de criação da classe de configuração do security. @Configuration e @EnableWebSecurity (a partir daqui o security vai ser interamente controlado por essa classe)

---
+++ src/main/java/com/projeto/app_hackaton/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.app_hackaton.repository;
+
+import com.projeto.app_hackaton.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
+}
