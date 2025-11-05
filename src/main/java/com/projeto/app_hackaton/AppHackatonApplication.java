18214b8d1f45fac4534e31ae0e3231523323f18e
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-11-05 12:20:59 -0300

    Setup inicial

---
+++ src/main/java/com/projeto/app_hackaton/AppHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.app_hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHackatonApplication.class, args);
+	}
+
+}
