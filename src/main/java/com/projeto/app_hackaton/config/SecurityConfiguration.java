bee5c4aa74fa7bf6be5b5a7efdffb78c889e6472
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-11-05 13:03:18 -0300

    Security #3
    Adicionado o método de filterChain, que será a camada de filtro de acesso da nossa aplicação, no momento vamos deixar tudo permitido, depois adicionamos as proibições...

--- src/main/java/com/projeto/app_hackaton/config/SecurityConfiguration.java
+++ src/main/java/com/projeto/app_hackaton/config/SecurityConfiguration.java
@@ -1,10 +1,23 @@
 package com.projeto.app_hackaton.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf-> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
